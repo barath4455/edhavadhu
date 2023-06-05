f6c08743edcd6796263568b4ccaaf1c700ad31f5
joshuaj0408 <joshuaj0408@gmail.com>
2023-06-05 11:36:19 +0530

    pushing to origin

---
+++ jenkinsing-maven/src/main/java/com/jenkins/jenkinsingmaven/JenkinsingMavenApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinsingmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsingMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsingMavenApplication.class, args);
+	}
+
+}
